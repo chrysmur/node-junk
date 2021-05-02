f07b477436074761d9a59f020b36e7212d032e03
chrysmur <sostomc011@gmail.com>
2021-05-03 00:46:05 +0300

    sharing data across pages adding products array

--- routes/admin.js
+++ routes/admin.js
@@ -6,13 +6,15 @@ import rootDir from '../helpers/path.js'
 const adminRouter = express.Router();
 const __dirname = path.resolve()
 
+const products = []; // We want to share data across calls
+
 adminRouter.get("/add-product", (req, res, next) => {
     res.sendFile(path.join(rootDir, "views", "add-product.html"))
 })
 
 adminRouter.post("/add-product", (req, res, next) => {
-    console.log(req.body)
+    products.push({ title: req.body.title })
     res.redirect("/")
 })
 
-export {adminRouter}
+export {adminRouter, products}
--- routes/shop.js
+++ routes/shop.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import path from  'path';
 
+import {products} from './admin.js'
 // By default __dirname resolves to the current folder
 // since __dirname is only defined in scripts, for REPL, we define it to path resolve to resolve to project root
 const __dirname = path.resolve() 
@@ -8,6 +9,7 @@ const __dirname = path.resolve()
 const shopRouter =  express.Router()
 
 shopRouter.get("/", (req, res, next) => {
+  console.log(products)
     res.sendFile(path.join(__dirname, 'views', 'shop.html')) // project root then views/shop.html
   });
   
